0c96b8c761d32368ecd7467f61e1cb4ed9ef48e5
Hugo Oyola <hoyola@ucv.edu.pe>
2024-09-06 17:43:46 -0500

    Refactorizar componentes y mejorar estilos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
 
+  get capitalizaName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} is ${this.age} years old`;
+  }
+
+  changeHero(): void {
+    this.name = 'spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
 }
