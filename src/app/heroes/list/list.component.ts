85c02d96cc9b9ea83c482c57b65c8533a18783d7
Hugo Oyola <hoyola@ucv.edu.pe>
2024-09-06 17:48:35 -0500

    Refactorizar componente de lista y mejorar estilos

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
-
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Captain America',
+  ];
 }
